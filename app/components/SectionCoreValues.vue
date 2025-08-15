63d375f5577e0999b753a0b14d15f9a72221aa9e
Boston Impressions <bostonimpressions@gmail.com>
2025-08-15 15:14:22 -0400

    core values section

---
+++ app/components/SectionCoreValues.vue
@@ -0,0 +1,71 @@
+<template>
+  <section class="core-values">
+    <div class="container">
+      <h2>Our <span>Core</span> Values</h2>
+      <ul class="items">
+        <li>RADICAL INTEGRITY (NO B.S.)</li>
+        <li class="blue">TRANSFORMATIVE VALUE</li>
+        <li>UNLOCKED COMMUNITY</li>
+        <li class="blue">ULTIMATE ACCOUNTABILITY</li>
+        <li>UNMATCHED COMMITMENT</li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+  .core-values {
+    padding: 121px 0 143px;
+    background-color: var(--DarkBlue);
+  }
+  .container {
+    max-width: 1088px;
+    margin: 0 auto;
+  }
+  h2 {
+    color: var(--OffWhite);
+    font-size: 48px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 122.631%; /* 58.863px */
+    letter-spacing: 2.4px;
+    text-transform: uppercase;
+    margin: 0 0 40px;
+
+    span {
+      color: var(--Copper);
+      margin: 0 0 40px;
+    }
+  }
+  ul {
+    display: flex;
+  }
+  li {
+    display: flex;
+    height: 233.6px;
+    padding: 86px 17px;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    flex: 1 0 0;
+    aspect-ratio: 1/1;
+    border-radius: 128.653px;
+    color: var(--OffWhite);
+    text-align: center;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 28px; /* 140% */
+    letter-spacing: 0.8px;
+    text-transform: uppercase;
+    background: rgba(227, 140, 59, 0.55);
+
+    &.blue {
+      background: rgba(103, 185, 223, 0.41);
+    }
+  }
+
+  @media (min-width: 768px) {
+  }
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-const { data: intro } = await useAsyncData(() =>
-  queryCollection('content')
-    .path('/home/intro')
-    .first()
-)
+  const { data: intro } = await useAsyncData(() =>
+    queryCollection('content').path('/home/intro').first()
+  )
 </script>
 
 <template>
@@ -14,9 +12,7 @@ const { data: intro } = await useAsyncData(() =>
           UNLOCKED HOME PROJECT IS <br />
           REVOLUTIONIZING REAL ESTATE.
         </h1>
-        <p class="subheading ">
-          EXPERIENCED. KNOWLEDGEABLE. TRUSTWORTHY.
-        </p>
+        <p class="subheading">EXPERIENCED. KNOWLEDGEABLE. TRUSTWORTHY.</p>
 
         <!-- Intro content -->
         <div class="intro-text">
@@ -25,86 +21,86 @@ const { data: intro } = await useAsyncData(() =>
         </div>
 
         <p class="cta">
-          CLICK BELOW TO LEARN ABOUT THE WAYS WE DELIVER EXCEPTIONAL SERVICE TO OUR CLIENTS.
+          CLICK BELOW TO LEARN ABOUT THE WAYS WE DELIVER EXCEPTIONAL SERVICE TO
+          OUR CLIENTS.
         </p>
         <AudienceCards />
       </section>
     </SectionHero>
+    <SectionCoreValues />
     <SectionContactForm />
   </div>
 </template>
 
 <style lang="scss" scoped>
-.home-intro {
-  display: flex;
-  margin: 0 auto;
-  flex-direction: column;
-  align-items: flex-start;
-  max-width: 1088px;
-  padding: 15px 0 91px 0;
-  gap: 20px;
-}
-.heading {
-  color: var(--Blue);
-  letter-spacing: 2.4px;
-  text-transform: uppercase;
-  font-style: normal;
-  font-weight: 600;
-  font-size: 24px;
-  line-height: 122.631%; /* 58.863px */
-  
-}
-.subheading {
-  color: var(--Copper);
-  font-style: normal;
-  font-weight: 500;
-  line-height: normal;
-  letter-spacing: 1.68px;
-  text-transform: uppercase;
-  font-size: 22px;
-}
-.intro-text {
-  color: var(--OffWhite);
-  font-style: normal;
-  font-weight: 400;
-  line-height: 28px;
-  letter-spacing: 0.8px;
-  font-size: 18px;
-}
-.cta {
-  color: var(--Copper);
-  font-style: normal;
-  font-weight: 600;
-  line-height: normal;
-  letter-spacing: 0.8px;
-  text-transform: uppercase;
-  max-width: 874px;
-  font-size: 20px;
-}
-
-@media (min-width: 768px) {
   .home-intro {
+    display: flex;
+    margin: 0 auto;
+    flex-direction: column;
+    align-items: flex-start;
     max-width: 1088px;
-    padding: 55px 0 91px 0;
-    gap: 33px;
+    padding: 15px 0 91px 0;
+    gap: 20px;
   }
   .heading {
-    font-size: 48px;
+    color: var(--Blue);
+    letter-spacing: 2.4px;
+    text-transform: uppercase;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 24px;
+    line-height: 122.631%; /* 58.863px */
   }
   .subheading {
-    font-size: 24px;
-    line-height: 32px;
-
+    color: var(--Copper);
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+    letter-spacing: 1.68px;
+    text-transform: uppercase;
+    font-size: 22px;
   }
   .intro-text {
-    font-size: 20px;
-    line-height: 38px; /* 190% */
+    color: var(--OffWhite);
+    font-style: normal;
+    font-weight: 400;
+    line-height: 28px;
+    letter-spacing: 0.8px;
+    font-size: 18px;
   }
   .cta {
-    font-size: 20px;
-    line-height: 30px;
-
+    color: var(--Copper);
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+    letter-spacing: 0.8px;
+    text-transform: uppercase;
     max-width: 874px;
+    font-size: 20px;
   }
-}
+
+  @media (min-width: 768px) {
+    .home-intro {
+      max-width: 1088px;
+      padding: 55px 0 91px 0;
+      gap: 33px;
+    }
+    .heading {
+      font-size: 48px;
+    }
+    .subheading {
+      font-size: 24px;
+      line-height: 32px;
+    }
+    .intro-text {
+      font-size: 20px;
+      line-height: 38px; /* 190% */
+    }
+    .cta {
+      font-size: 20px;
+      line-height: 30px;
+
+      max-width: 874px;
+    }
+  }
 </style>
